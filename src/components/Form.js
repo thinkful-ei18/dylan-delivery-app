4f46e557903b1e03d0bfd72f63c0bbf34ea3affd
Dylan <nielsen.dylan@gmail.com>
2018-03-07 09:02:01 -0800

    Install redux-form and update form with redux-form components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import { reduxForm, Field } from "redux-form";
 
-export default function Form(props) {
+export function Form(props) {
+  function onSubmit(values) {
+    console.log(values);
+  }
   return <div>
       <form>
-        <label>Tracking Number</label>
-        <input type="text"/>
-        <label>What is your issue</label>
-        <select name="text" id="complaint-reason">
+        <label htmlFor="tracking">Tracking Number</label>
+        <Field name="tracking" component="input" type="text"/>
+        <label htmlFor="complaint">What is your issue</label>
+        <Field component="select" name="complaint" id="complaint-reason">
           <option value="no-arrival">My Delivery Hasn't Arrived</option>
           <option value="wrong-item">The wrong item was delivered</option>
           <option value="part-missing">Part of my order was missing</option>
           <option value="damaged">Some of my order was damaged</option>
           <option value="other">Other (give details below)</option>
-        </select>
-        <label>Give more details</label>
-        <textarea name="details" id="complaint-details" cols="30" rows="10"></textarea>
+        </Field>
+        <label htmlFor="details">Give more details</label>
+        <Field component="textarea" name="details" id="complaint-details" cols="30" rows="10"></Field>
         <input type="submit"></input>
       </form>
     </div>;
 }
+
+export default reduxForm({
+  form: 'delivery-complaint'
+})(Form);
